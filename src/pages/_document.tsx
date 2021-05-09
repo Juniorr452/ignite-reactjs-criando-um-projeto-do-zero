c01b3ca65ee6f404333e3a2f62fcb4f42671386f
Juniorr452 <juniorr452@gmail.com>
2021-05-09 16:09:26 -0300

    Moving the Header component out of document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Header from '../components/Header';
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
@@ -13,7 +12,6 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
-          <Header />
           <Main />
           <NextScript />
         </body>
