3d509489856e6e25abb8b1cfe6ad9c8921a0a315
Juniorr452 <juniorr452@gmail.com>
2021-05-09 21:44:35 -0300

    Pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Prismic from '@prismicio/client';
 import { FaCalendar, FaUser } from 'react-icons/fa';
 
 import dateFormat from 'src/utils/dateFormat';
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -27,18 +28,29 @@ interface PostPagination {
 }
 
 interface HomeProps {
-  posts: Post[];
   postsPagination: PostPagination;
 }
 
-const Home: React.FC<HomeProps> = ({ posts, postsPagination }) => {
+const Home: React.FC<HomeProps> = ({ postsPagination }) => {
+  const [posts, setPosts] = useState(postsPagination);
+
+  const handleLoadMorePosts = async (): Promise<void> => {
+    const response = await fetch(posts.next_page);
+    const data: PostPagination = await response.json();
+
+    setPosts({
+      next_page: data.next_page,
+      results: [...posts.results, ...data.results],
+    });
+  };
+
   return (
     <main className={commonStyles.container}>
       <Head>
         <title>Postagens | spacetravelling</title>
       </Head>
       <div className={styles.posts}>
-        {posts.map(post => (
+        {posts.results.map(post => (
           <article key={post.uid}>
             <Link href={`/post/${post.uid}`}>
               <a>
@@ -58,6 +70,16 @@ const Home: React.FC<HomeProps> = ({ posts, postsPagination }) => {
             </div>
           </article>
         ))}
+
+        {posts.next_page && (
+          <button
+            type="button"
+            onClick={handleLoadMorePosts}
+            className={styles.carregarMais}
+          >
+            Carregar mais posts
+          </button>
+        )}
       </div>
     </main>
   );
@@ -67,12 +89,12 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
   const postsResponse = await prismic.query(
     Prismic.Predicates.at('document.type', 'posts'),
-    { orderings: '[my.posts.date desc]' }
+    { orderings: '[my.posts.date desc]', pageSize: 1 }
   );
 
   return {
     props: {
-      posts: postsResponse.results,
+      postsPagination: postsResponse,
     },
   };
 };
